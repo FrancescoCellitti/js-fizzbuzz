34d5f6cb7e0713f7ad6f2e9f81aa0e392e4af752
FrancescoCellitti <francescocellitti@virgilio.it>
2025-05-28 15:59:36 +0200

    Enhance FizzBuzz output to include numbers alongside messages

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
+//inizializzo il for con la variabile che viene incrementata da 1 a 100
 for (i = 1; i <= 100; i++) {
+    //metto la condizione se il numero è divisibile per entrambi con il conseguente messaggio
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FizzBuzz")
+        console.log(i, "FizzBuzz")
+    //altrimenti se non è divisibile metto un altra condizione, 
+    // ovvero, se è divisibile solo per 3 con il conseguente messaggio
     } else if (i % 3 == 0) {
-        console.log("Fizz")
-
+        console.log(i, "Fizz")
+    //altra condizione nel caso in cui non fosse divisibile ne per entrambi ne per 3 ma fosse
+    //per 5 con conseguente messaggio
     } else if (i % 5 == 0) {
-        console.log("Buzz")
+        console.log(i, "Buzz")
+    //altrimenti stammpa il semplice valore di i 
     } else {
         console.log(i)
     }
